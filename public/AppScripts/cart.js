44e2ba1b744ee11e302981007e42407d6088f27c
levybeny <ySsdsp9j>
2017-08-05 23:50:59 +0300

    jj

--- public/AppScripts/cart.js
+++ public/AppScripts/cart.js
@@ -126,8 +126,6 @@ app.factory('CartService', ['$http', '$window', function ($http, $window) {
             service.cart[index].buyQuantity = service.quantity[index];
             service.cart[index].totalPrice = service.cart[index].buyQuantity * service.cart[index].price;
         }
-
-
         service.updateCart();
 
     }
